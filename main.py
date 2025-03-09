fbaa34fd496c2c80a5ed3cb73bf19a08bff8f6b8
Anzhelica <angelica.sobol@gmail.com>
2025-03-09 14:34:08 +0300

    Часть №5

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import sys
 import os
 
+import requests
 from PyQt6 import uic
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
-from PyQt6.QtWidgets import QMainWindow, QLabel, QApplication, QRadioButton
+from PyQt6.QtWidgets import QMainWindow, QLabel, QApplication, QRadioButton, QLineEdit, QPushButton, QStatusBar
 from utilities2 import get_static_api_image
+
+API_KEY = "8013b162-6b42-4997-9691-77b7074026e0"
 
 
 class MainWindow(QMainWindow):
     map_label: QLabel
     dark_theme: QRadioButton
     light_theme: QRadioButton
+    lineEdit: QLineEdit
+    search_button: QPushButton
+    mainWindow: QMainWindow
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -22,9 +28,15 @@ class MainWindow(QMainWindow):
         self.refresh_map()
         self.dark_theme.clicked.connect(self.change_theme)
         self.light_theme.clicked.connect(self.change_theme)
+        self.search_button.clicked.connect(self.search_address)
+        self.lineEdit.setPlaceholderText('Введит адрес')
 
-    def refresh_map(self):
-        response = get_static_api_image(self.map, self.z, self.theme)
+        self.statusbar = QStatusBar(self)
+        self.statusbar.move(20, 500)
+        self.statusbar.resize(400, 30)
+
+    def refresh_map(self, flag=False):
+        response = get_static_api_image(self.map, self.z, self.theme, True if flag else False)
         if response:
             with open('tmp.png', mode='wb') as tmp:
                 tmp.write(response)
@@ -36,23 +48,48 @@ class MainWindow(QMainWindow):
     def keyPressEvent(self, event):
         key = event.key()
         n = 0.01 if self.z in range(13, 17) else 0.1 if self.z in range(8, 12) else 1 if range(3, 7) else 0
-        if key == Qt.Key.Key_PageUp:
-            self.z += 1 if self.z < 19 else 0
-        elif key == Qt.Key.Key_PageDown:
-            self.z -= 1 if self.z > 6 else 0
-        elif key == Qt.Key.Key_Left:
-            self.map[0] -= n
-        elif key == Qt.Key.Key_Right:
-            self.map[0] += n
-        elif key == Qt.Key.Key_Up:
-            self.map[1] += n
-        elif key == Qt.Key.Key_Down:
-            self.map[1] -= n
-        self.refresh_map()
+        if key == Qt.Key.Key_Enter:
+            self.search_address()
+        else:
+            if key == Qt.Key.Key_PageUp:
+                self.z += 1 if self.z < 19 else 0
+            elif key == Qt.Key.Key_PageDown:
+                self.z -= 1 if self.z > 2 else 0
+            elif key == Qt.Key.Key_Left:
+                self.map[0] -= n
+            elif key == Qt.Key.Key_Right:
+                self.map[0] += n
+            elif key == Qt.Key.Key_Up:
+                self.map[1] += n
+            elif key == Qt.Key.Key_Down:
+                self.map[1] -= n
+            self.refresh_map()
 
     def change_theme(self):
         self.theme = 'dark' if self.theme == 'light' else 'light'
         self.refresh_map()
+
+    def search_address(self):
+        address = self.lineEdit.text()
+        geo_request = f"http://geocode-maps.yandex.ru/1.x/?apikey={API_KEY}&geocode={address}&format=json"
+        if address:
+            self.statusbar.showMessage('') if self.statusbar.currentMessage() != '' else False
+
+            try:
+                response = requests.get(geo_request)
+                if response:
+                    json_response = response.json()
+                else:
+                    return None
+                features = json_response['response']['GeoObjectCollection']['featureMember']
+                t = features[0]['GeoObject'] if features else None
+                coords = t["Point"]["pos"]
+                self.map = [float(coords.split()[0]), float(coords.split()[1])]
+                self.refresh_map(True)
+            except:
+                self.statusbar.showMessage('Вы ввели неправельный адрес')
+        else:
+            self.statusbar.showMessage('Вы ввели неправельный адрес')
 
 
 if __name__ == '__main__':
--- utilities2.py
+++ utilities2.py
@@ -5,13 +5,14 @@ from urllib3 import Retry
 API_KEY = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
 
 
-def get_static_api_image(ll, z, theme):
+def get_static_api_image(ll, z, theme, flag=False):
     map_api_server = 'https://static-maps.yandex.ru/v1?'
     map_params = {
         'll': ','.join(map(str, ll)),
         'apikey': API_KEY,
         'theme': theme,
-        'z': z
+        'z': z,
+        'pt': "~".join(["{0},pm2dgl".format(','.join(map(str, ll)) if flag else '0,0')])
     }
     session = requests.Session()
     retry = Retry(total=10, connect=5, backoff_factor=0.5)
